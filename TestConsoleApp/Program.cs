7638bd247aac164e75dba5717cdbb1acadcad0d0
Farooq Mahmud <farooq@teqniqly.com>
2020-11-05 14:44:43 -0800

    Console app

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder
+                .AddAzureAppConfiguration(Environment
+                    .GetEnvironmentVariable("ConnectionString"));
+
+            var config = builder.Build();
+            Console
+                .WriteLine(config["TestConsoleApp:Settings:Message"]
+                    ?? "Hello world!");
+        }
+    }
+}
