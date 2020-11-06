b067bc9c83bd5af0c0c2e72d280fd5e4802d73a7
Farooq <farooq@teqniqly.com>
2020-11-06 12:49:10 -0800

    Add/get config settings using SDK

---
+++ WebAppWithKeyVault/WebAppWithKeyVault/Models.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace WebAppWithKeyVault
+{
+    public class Header
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Context Context { get; set; }
+    }
+
+    public class Context
+    {
+        public string Tenant { get; set; }
+
+        public IEnumerable<MetaDatum> Metadata { get; set; }
+    }
+
+    public class MetaDatum
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
