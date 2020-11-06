2b4e643917c9f830cfc2308c20ca654d2c4ec93b
Farooq <farooq@teqniqly.com>
2020-11-06 10:47:54 -0800

    App config service - web API with key vault

---
+++ WebAppWithKeyVault/WebAppWithKeyVault/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebAppWithKeyVault.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IOptions<ApiConfiguration> _configurationOptions;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(IOptions<ApiConfiguration> configurationOptions, ILogger<WeatherForecastController> logger)
+        {
+            _configurationOptions = configurationOptions;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Configuration = new ApiConfiguration {Message = _configurationOptions.Value.Message, SecretMessage = _configurationOptions.Value.SecretMessage}
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebAppWithKeyVault/WebAppWithKeyVault/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAppWithKeyVault
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.Configure<ApiConfiguration>(apiConfiguration =>
+            {
+                apiConfiguration.Message = this.Configuration["TestConsoleApp:Settings:Message"];
+                apiConfiguration.SecretMessage = this.Configuration["TestApp:Settings:KeyVaultMessage"];
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
