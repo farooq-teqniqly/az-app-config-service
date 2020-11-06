d3d1fe452d22b633a2d10367e5d0208c65a5e6e9
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-11-06 10:48:50 -0800

    Azure App Config service examples

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        private static IConfiguration _configuration = null;
+
+        private static IConfigurationRefresher _refresher = null;
+
+        private static string settingKey = "TestConsoleApp:Settings:Message";
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder
+                .AddAzureAppConfiguration(options =>
+                {
+                    options
+                        .Connect(Environment
+                            .GetEnvironmentVariable("ConnectionString"))
+                        .ConfigureRefresh(refresh =>
+                        {
+                            refresh
+                                .Register(settingKey)
+                                .SetCacheExpiration(TimeSpan.FromSeconds(10));
+                        });
+
+                    _refresher = options.GetRefresher();
+                });
+
+            _configuration = builder.Build();
+            PrintMessage().Wait();
+        }
+
+        private static async Task PrintMessage()
+        {
+            Console
+                .WriteLine(_configuration[settingKey]
+                    ?? "Hello world!");
+
+            // Wait for the user to press Enter
+            Console.ReadLine();
+
+            await _refresher.TryRefreshAsync();
+            Console
+                .WriteLine(_configuration[settingKey]
+                    ?? "Hello world!");
+        }
+    }
+}
---
+++ WebAppWithKeyVault/WebAppWithKeyVault/ApiConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppWithKeyVault
+{
+    public class ApiConfiguration
+    {
+        public string Message { get; set; }
+        public string SecretMessage { get; set; }
+    }
+}
